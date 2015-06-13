7bdeb449b0d44107cbf0501f27fbb6ebdfe82c85
barstuk <barstuk.ror@gmail.com>
2015-06-13 16:11:48 +0200

    added partials

--- app/assets/javascripts/angular/controllers/mentorAccountCtrl.js
+++ app/assets/javascripts/angular/controllers/mentorAccountCtrl.js
@@ -1,7 +1,12 @@
 angular.module('app').controller('MentorAccountCtrl', ['$scope', function($scope) {
-  $scope.emailValidator = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
   var user = gon.user
+  $scope.user = {}
   if (user){
-    $scope.user = angular.fromJson(user)
+    user = angular.fromJson(user)
+    $scope.user.email = user.email
+    $scope.user.first_name = user.first_name
+    $scope.user.last_name = user.last_name
+    $scope.user.funding_email = user.email
+
   }
 }]);
