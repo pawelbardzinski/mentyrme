9098db250266be340aaae1b0051171427bad5866
barstuk <barstuk.ror@gmail.com>
2015-06-18 19:32:49 +0200

    partialy merged

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,8 @@ Rails.application.routes.draw do
   post 'payment/mentor_save',   to: "accounts#mentor_save",               as: "save_mentor"
   get 'payment/card_added',     to: "accounts#card_added",                as: "card_added"
   get 'transaction/index',      to: "transactions#index",                 as: "transaction_index"
-
+  get 'payment/release_escrow',to: "accounts#release_escrow",            as: "release_escrow"
+  
   get 'profiles/:id', to: "profiles#show", as: "public_profile"
   resource :profile, only: [:edit, :update] do
     get 'photos',     to: "profiles#photos", as: "photos"
@@ -47,7 +48,7 @@ Rails.application.routes.draw do
   resources :bookings
 
   resources :photos, only: [:destroy]
-   
+
 
   get 'find-mentor',  to: "dashboard#find_mentor"
   get 'my_sessions',   to: "dashboard#my_sessions"
@@ -55,19 +56,19 @@ Rails.application.routes.draw do
   match 'find-mentors-browse',  to: "bookings#find_mentors_browse", as: "booking_find_mentors_browse", via: [:get, :post]
 
   match 'find-mentor-details',  to: "bookings#find_mentor_details", as: "booking_find_mentor_details", via: [:get, :post]
-  match 'find-mentor-locations',  to: "bookings#find_mentor_locations", as: "booking_find_mentor_locations", via: [:get, :post] 
+  match 'find-mentor-locations',  to: "bookings#find_mentor_locations", as: "booking_find_mentor_locations", via: [:get, :post]
   match 'find-mentor-notes',  to: "bookings#find_mentor_notes", as: "booking_find_mentor_notes", via: [:get, :post]
-  match 'find-mentor-review',  to: "bookings#find_mentor_review", as: "booking_find_mentor_review", via: [:get, :post]  
+  match 'find-mentor-review',  to: "bookings#find_mentor_review", as: "booking_find_mentor_review", via: [:get, :post]
   match 'find-mentor-finalize',  to: "bookings#find_mentor_finalize", as: "booking_find_mentor_finalize", via: [:get, :post]
-    
-  
+
+
   match 'mentor-approve',  to: "bookings#mentor_approve", as: "booking_mentor_approve", via: [:get, :post]
   match 'mentor-reject',  to: "bookings#mentor_reject", as: "booking_mentor_reject", via: [:get, :post]
   match 'mentor-modification',  to: "bookings#mentor_modification", as: "booking_mentor_modification", via: [:get, :post]
-  
+
   match 'mentee-modification-approve',  to: "bookings#mentee_modification_approve", as: "booking_mentee_modification_approve", via: [:get, :post]
   match 'mentee-modification-reject',  to: "bookings#mentee_modification_reject", as: "booking_mentee_modification_reject", via: [:get, :post]
-  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
